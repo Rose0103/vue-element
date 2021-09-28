fd08c1832544d666d2f7320b26b1ecb3805a76f9
Rose0103 <1604857467@qq.com>
2021-09-28 22:23:03 +0800

    初始化

---
+++ src/components/CommonForm.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="home">
+    <h1>from表单封装</h1>
+    <!-- 标签显示名称 -->
+    <el-form ref="form" :model="form" label-width="100px" :inline="inline">
+      <el-form-item v-for="item in formLabel" :key="item.label" :label="item.label">
+          <!-- 根据type来显示什么标签 -->
+          <el-input :placeholder="'请输入' + item.label" v-if="item.type === 'input'" v-model="form[item.model]"></el-input>
+          <el-input :placeholder="'请输入' + item.label" v-else-if="item.type === 'textarea'" v-model="form[item.model]" type="textarea"></el-input>
+           <el-select placeholder="请选择" v-if="item.type === 'select'"  v-model="form[item.model]">
+            <el-option v-for="item in item.opts" :key="item.value" :label="item.label" :value="item.value" ></el-option>
+          </el-select>
+          <el-switch v-if="item.type ==='switch'"  v-model="form[item.model]"></el-switch>
+          <el-checkbox-group v-if="item.type === 'checkbox'" v-model="form[item.model]">
+            <el-checkbox v-for="item in item.checkBox" :key="item.id" :label="item.title" name="type"></el-checkbox>
+          </el-checkbox-group>
+          <el-radio-group v-model="form[item.model]" v-if="item.type ==='radio'">
+            <el-radio v-for="item in item.radioBox"  :key="item.value" :label="item.label" :value='item.value'></el-radio>
+          </el-radio-group>
+          <el-date-picker
+            v-model="form[item.model]"
+            v-if="item.type ==='date'"
+            type="date"
+            value-format='yyyy-MM-dd'
+            placeholder="选择日期">
+          </el-date-picker>
+      </el-form-item>
+      <el-form-item><slot></slot></el-form-item>
+    </el-form>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Home',
+  components: {
+  },
+  props: {
+    formLabel: Array,
+    form: Object,
+    inline: Boolean
+  },
+  data () {
+    return {
+    }
+  },
+  methods: {
+  }
+}
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,121 @@
+<template>
+  <div class="home">
+    <common-form :formLabel="formLabel" :form="form" :inline="false">
+      <el-button type="primary" @click="submit()">提交</el-button>
+    </common-form>
+    <common-table :tableData="tableData"></common-table>
+  </div>
+</template>
+
+<script>
+import CommonForm from '../components/CommonForm.vue'
+import CommonTable from '../components/CommonTable.vue'
+export default {
+  name: 'Home',
+  components: {
+    CommonForm,
+    CommonTable
+  },
+  data () {
+    return {
+      formLabel: [
+        {
+          model: 'name',
+          label: '姓名',
+          type: 'input'
+        },
+        {
+          model: 'createDate',
+          label: '创建日期',
+          type: 'date'
+        },
+        {
+          model: 'department',
+          label: '部门',
+          type: 'select',
+          opts: [
+            {
+              label: '技术部',
+              value: 1
+            },
+            {
+              label: '财务部',
+              value: 2
+            }
+          ]
+        },
+        {
+          model: 'enable',
+          label: '是否启用',
+          type: 'switch'
+        },
+        {
+          model: 'hobby',
+          label: '兴趣爱好',
+          type: 'checkbox',
+          checkBox: [
+            {
+              id: 1,
+              title: '唱歌'
+            },
+            {
+              id: 2,
+              title: '逛街'
+            },
+            {
+              id: 3,
+              title: '看书'
+            }
+
+          ]
+        },
+        {
+          model: 'sex',
+          label: '性别',
+          type: 'radio',
+          radioBox: [
+            { label: '男', value: 1 },
+            { label: '女', value: 0 }
+          ]
+        },
+        {
+          model: 'remarks',
+          label: '备注',
+          type: 'textarea'
+        }
+      ],
+      form: {
+        name: '',
+        department: '',
+        enable: false,
+        hobby: [],
+        sex: '',
+        remarks: '',
+        createDate: ''
+      },
+      tableData: [{
+        date: '2016-05-02',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1518 弄'
+      }, {
+        date: '2016-05-04',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1517 弄'
+      }, {
+        date: '2016-05-01',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1519 弄'
+      }, {
+        date: '2016-05-03',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1516 弄'
+      }]
+    }
+  },
+  methods: {
+    submit () {
+      console.log(this.form)
+    }
+  }
+}
+</script>
